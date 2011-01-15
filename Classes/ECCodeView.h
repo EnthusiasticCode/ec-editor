40c1526caaf281e890c5a4ebe50d29d975d2f4fb
Nikso <thenikso@gmail.com>
2011-01-15 18:44:49 +0100

    Starting to fork omniui code to produce propertary control: ECCodeView.

---
+++ Classes/ECCodeView.h
@@ -0,0 +1,16 @@
+//
+//  ECCodeView.h
+//  edit
+//
+//  Created by Nicola Peduzzi on 15/01/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "OUIEditableFrame.h"
+
+@interface ECCodeView : OUIEditableFrame {
+
+}
+
+@end
---
+++ Classes/ECCodeView.m
@@ -0,0 +1,29 @@
+//
+//  ECCodeView.m
+//  edit
+//
+//  Created by Nicola Peduzzi on 15/01/11.
+//  Copyright 2011 Enthusiastic Code. All rights reserved.
+//
+
+#import "ECCodeView.h"
+
+
+@implementation ECCodeView
+
+
+- (id)initWithFrame:(CGRect)frame 
+{
+    if ((self = [super initWithFrame:frame])) 
+    {
+        
+    }
+    return self;
+}
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+
+@end
