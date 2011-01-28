646c969832bc88cba40552bbd8b1f4f3f6f80de2
Uri Baghin <skerfat@gmail.com>
2011-01-28 19:38:58 +0100

    Fixed clang parameters for iOS source files.

--- Classes/ECCodeProjectController.m
+++ Classes/ECCodeProjectController.m
@@ -53,8 +53,14 @@
     NSString *fileName = [[self contentsOfRootDirectory] objectAtIndex:indexPath.row];
     NSString *filePath = [self.project.rootDirectory stringByAppendingPathComponent:fileName];
     NSString *fileContents = [NSString stringWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error:nil];
+    // TODO: parameters are a bit screwed for now
+    // exclude standard and builtin include directories to isolate headers
+//    int parameter_count = 3;
+//    const char const *parameters[] = {"-nostdinc", "-nobuiltininc", "-I/Xcode4/SDKs/MacOSX10.6.sdk/usr/include/"};
+    int parameter_count = 10;
+    const char const *parameters[] = {"-ObjC", "-nostdinc", "-nobuiltininc", "-I/Xcode4//usr/lib/clang/2.0/include", "-I/Xcode4//Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator4.2.sdk/usr/include", "-F/Xcode4/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator4.2.sdk/System/Library/Frameworks", "-isysroot=/Xcode4/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator4.2.sdk/", "-DTARGET_OS_IPHONE=1", "-UTARGET_OS_MAC", "-miphoneos-version-min=4.2"};
     self.codeView.text = fileContents;
-    CXTranslationUnit TranslationUnit = clang_parseTranslationUnit(self.project.Index, [filePath cStringUsingEncoding:NSUTF8StringEncoding], 0, 0, 0, 0, CXTranslationUnit_None);
+    CXTranslationUnit TranslationUnit = clang_parseTranslationUnit(self.project.Index, [filePath cStringUsingEncoding:NSUTF8StringEncoding], parameters, parameter_count, 0, 0, CXTranslationUnit_None);
     int numDiagnostics = clang_getNumDiagnostics(TranslationUnit);
     for (int i = 0; i < numDiagnostics; i++)
     {
@@ -62,6 +68,7 @@
         CXString String = clang_formatDiagnostic(Diagnostic, clang_defaultDiagnosticDisplayOptions());
         NSLog(@"%s", clang_getCString(String));
         clang_disposeString(String);
+        clang_disposeDiagnostic(Diagnostic);
     }
     clang_disposeTranslationUnit(TranslationUnit);
 }
