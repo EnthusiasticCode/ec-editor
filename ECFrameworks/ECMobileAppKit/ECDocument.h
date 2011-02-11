61bdef47a86bedd8d6606d687e6c60083050fd15
Uri Baghin <skerfat@gmail.com>
2011-02-11 16:30:25 +0100

    Moved some stuff around.

---
+++ ECFrameworks/ECMobileAppKit/ECDocument.h
@@ -0,0 +1,17 @@
+//
+//  ECDocument.h
+//  edit
+//
+//  Created by Uri Baghin on 2/11/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface ECDocument : NSObject {
+@private
+    
+}
+
+@end
---
+++ ECFrameworks/ECMobileAppKit/ECDocument.m
@@ -0,0 +1,14 @@
+//
+//  ECDocument.m
+//  edit
+//
+//  Created by Uri Baghin on 2/11/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "ECDocument.h"
+
+
+@implementation ECDocument
+
+@end
