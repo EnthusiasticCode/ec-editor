2d84333e5b8efd17ad60e172ee1861a8f25639a5
Uri Baghin <skerfat@gmail.com>
2011-03-04 18:19:59 +0100

    Added ECHashNSUIntegers.

---
+++ Frameworks/ECAdditions/ECHashing.h
@@ -0,0 +1,11 @@
+//
+//  ECHashing.h
+//  ECAdditions
+//
+//  Created by Uri Baghin on 3/4/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NSUInteger ECHashNSUIntegers(NSUInteger *values, NSUInteger count);
