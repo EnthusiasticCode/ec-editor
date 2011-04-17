46eb5905f5aae5b46ca560469bc0db162cfcf517
Nikso <thenikso@gmail.com>
2011-04-17 11:32:17 +0200

    Completed first implementation of new internal structures for text renderer.

--- Apps/CodeView3/CodeView3/ECTextRenderer.m
+++ Apps/CodeView3/CodeView3/ECTextRenderer.m
@@ -19,9 +19,10 @@
 //    > frame info cache: that cache wrap sizes to frame infos;
 //    > actual size: cache the actual size of rendered text.
 
+#pragma mark -
 #pragma mark TextSegmentFrame
 
-@interface TextSegmentFrame : NSObject
+@interface TextSegmentFrame : NSObject //<NSDiscardableContent>
 
 @property (nonatomic, readonly) CTFrameRef frame;
 
@@ -31,10 +32,77 @@
 
 - (id)initWithFramesetter:(CTFramesetterRef)framesetter wrapWidth:(CGFloat)width;
 
-- (void)releaseFrame;
+@end
+
+@implementation TextSegmentFrame
+
+@synthesize frame, wrapWidth, height;
+
+- (id)initWithFramesetter:(CTFramesetterRef)framesetter wrapWidth:(CGFloat)width
+{
+    if ((self = [super init]) && framesetter)
+    {
+        // Create path
+        wrapWidth = width;
+        CGMutablePathRef path = CGPathCreateMutable();
+        CGPathAddRect(path, NULL, (CGRect){ CGPointZero, { width, CGFLOAT_MAX } });
+        
+        // Create frame
+        frame = CTFramesetterCreateFrame(framesetter, (CFRange){ 0, 0 }, path, NULL);
+        CGPathRelease(path);
+        
+        // Calculate actual height
+        height = 0;
+        CFArrayRef lines = CTFrameGetLines(frame);
+        CFIndex lineCount = CFArrayGetCount(lines);
+        CTLineRef line;
+        CGFloat ascent, descent;
+        for (CFIndex i = 0; i < lineCount; ++i)
+        {
+            line = CFArrayGetValueAtIndex(lines, i);
+            CTLineGetTypographicBounds(line, &ascent, &descent, NULL);
+            height += ascent + descent;
+        }
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+    if (frame) 
+    {
+        CFRelease(frame);
+    }
+    [super dealloc];
+}
+
+//- (BOOL)beginContentAccess
+//{
+//    return frame != NULL;
+//}
+//
+//- (void)endContentAccess
+//{
+//    
+//}
+//
+//- (void)discardContentIfPossible
+//{
+//    if (frame) 
+//    {
+//        CFRelease(frame);
+//        frame = NULL;
+//    }
+//}
+//
+//- (BOOL)isContentDiscarded
+//{
+//    return frame == NULL;
+//}
 
 @end
 
+#pragma mark -
 #pragma mark TextSegment
 
 @interface TextSegment : NSObject {
@@ -66,6 +134,7 @@
 - (void)setTextSegmentGeneratingString:(NSAttributedString *)string startingAtLocation:(NSUInteger)location;
 
 /// Enumerate all the rendered lines in the text segment that intersect the given rect. 
+/// The rect should be relative to this segment coordinates.
 /// The block to apply will receive the line and its bounds relative to the first rendered
 /// line in this segment.
 - (void)enumerateLinesIntersectingRect:(CGRect)rect usingBlock:(void(^)(CTLineRef line, CGRect lineBound, BOOL *stop))block;
@@ -73,6 +142,110 @@
 /// Release framesetters and frames to reduce space consumption. To release the frame
 /// this method will actually clear the framse cache.
 - (void)releaseFramesetterAndFrames;
+
+@end
+
+@implementation TextSegment
+
+#pragma mark TextSegment Properties
+
+@synthesize stringRange, renderWrapWidth;
+
+- (void)setRenderWrapWidth:(CGFloat)width
+{
+    if (renderWrapWidth != width) 
+    {
+        renderWrapWidth = width;
+        [widthFramesCache removeAllObjects];
+    }
+}
+
+- (TextSegmentFrame *)frameForCurrentWidth
+{
+    NSNumber *wrapWidth = [NSNumber numberWithFloat:renderWrapWidth];
+    TextSegmentFrame *segmentFrame = [widthFramesCache objectForKey:wrapWidth];
+    if (!segmentFrame) 
+    {
+        if (!framesetter)
+            return nil;
+        segmentFrame = [[TextSegmentFrame alloc] initWithFramesetter:framesetter wrapWidth:renderWrapWidth];
+        [widthFramesCache setObject:segmentFrame forKey:wrapWidth];
+    }
+    return segmentFrame;
+}
+
+- (CGFloat)renderHeight
+{
+    return self.frameForCurrentWidth.height;
+}
+
+#pragma mark TextSegment Methods
+
+- (id)init 
+{
+    if ((self = [super init])) 
+    {
+        widthFramesCache = [NSCache new];
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+    [self releaseFramesetterAndFrames];
+    [widthFramesCache release];
+    [super dealloc];
+}
+
+- (void)releaseFramesetterAndFrames
+{
+    if (framesetter) 
+    {
+        CFRelease(framesetter);
+        framesetter = NULL;
+    }
+    [widthFramesCache removeAllObjects];
+}
+
+// TODO make a delegate to be sure to call this method if framsetter is NULL
+- (void)setTextSegmentGeneratingString:(NSAttributedString *)string startingAtLocation:(NSUInteger)location
+{
+    [self releaseFramesetterAndFrames];
+    framesetter = CTFramesetterCreateWithAttributedString((CFAttributedStringRef)string);
+    stringRange = NSMakeRange(location, [string length]);
+}
+
+- (void)enumerateLinesIntersectingRect:(CGRect)rect usingBlock:(void (^)(CTLineRef, CGRect, BOOL *))block
+{
+    if (CGRectIsNull(rect) || CGRectIsEmpty(rect)) 
+    {
+        rect = CGRectInfinite;
+    }
+    CGFloat rectEnd = CGRectGetMaxY(rect);
+    
+    BOOL stop = NO;
+    CGFloat currentY = 0;
+    CFArrayRef lines = CTFrameGetLines(self.frameForCurrentWidth.frame);
+    CFIndex lineCount = CFArrayGetCount(lines);
+    CGFloat width, ascent, descent;
+    CGRect bounds;
+    for (CFIndex i = 0; i < lineCount; ++i) 
+    {
+        CTLineRef line = CFArrayGetValueAtIndex(lines, i);
+        width = CTLineGetTypographicBounds(line, &ascent, &descent, NULL);
+        bounds = CGRectMake(0, currentY, width, ascent + descent);
+        if (currentY + bounds.size.height > rect.origin.y) 
+        {
+            // Break if past the required rect
+            if (currentY >= rectEnd)
+                break;
+            //
+            block(line, bounds, &stop);
+            if (stop) break;
+        }
+        currentY += bounds.size.height;
+    }
+}
 
 @end
 
@@ -85,10 +258,13 @@
 }
 @end
 
-#pragma mark Implementation
 
 @implementation ECTextRenderer
 
+#pragma mark Properties
+
 @synthesize delegate, datasource, maximumLineCountPerSegment, lazyCaching, wrapWidth;
+
+#pragma mark Public Methods
 
 @end
