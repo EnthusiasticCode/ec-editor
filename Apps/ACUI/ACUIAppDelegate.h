4c233fff633d01379587a980751df23ec69b3321
Nikso <thenikso@gmail.com>
2011-06-14 11:38:57 +0200

    And adding actual files

---
+++ Apps/ACUI/ACUIAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ACUIAppDelegate.h
+//  ACUI
+//
+//  Created by Nicola Peduzzi on 09/06/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ACUIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
