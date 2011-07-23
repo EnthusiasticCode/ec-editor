fbb386630f223ce2a3830b8db5c0390cb468a0cd
Nikso <thenikso@gmail.com>
2011-07-23 17:03:02 +0200

    Starting to work on beziel alert controller.

---
+++ Frameworks/ECUIKit/ECBezelAlert.h
@@ -0,0 +1,28 @@
+//
+//  ECBezelAlert.h
+//  ArtCode
+//
+//  Created by Nicola Peduzzi on 23/07/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/// Shows a bezel styled alert for a given time.
+@interface ECBezelAlert : UIViewController
+
+/// Return a shared instance of the bezel alert.
++ (ECBezelAlert *)sharedAlert;
+
+/// The time for which the alert stays vivible before faigin out. Default 1 second.
+@property (nonatomic) NSTimeInterval visibleTimeInterval;
+
+/// Call this method to post a new alert message that will be displayed after the remaining posted messages.
+/// If the immediate flag is YES, the alert queue will be cleared and this message will be shown immediatly.
+/// The view controller may use the contentSizeForViewInPopover property to set the size of the bezel alert.
+- (void)addAlertMessageWithViewController:(UIViewController *)viewController displayImmediatly:(BOOL)immediate;
+
+/// Conviniance method to add a message with an image and a text under it.
+- (void)addAlertMessageWithText:(NSString *)text image:(UIImage *)image displayImmediatly:(BOOL)immediate;
+
+@end
---
+++ Frameworks/ECUIKit/ECBezelAlert.m
@@ -0,0 +1,34 @@
+//
+//  ECBezelAlert.m
+//  ArtCode
+//
+//  Created by Nicola Peduzzi on 23/07/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "ECBezelAlert.h"
+#import <QuartzCore/QuartzCore.h>
+
+#import "UIImage+BlockDrawing.h"
+
+@implementation ECBezelAlert
+@synthesize visibleTimeInterval;
+
+#pragma mark - View Lifecycle
+
+- (void)loadView
+{
+    // View background image
+    CGFloat radius = 10;
+    UIImage *bezieBackgroundImage = [[UIImage imageWithSize:CGSizeMake(radius * 2 + 1, radius * 2 + 1) block:^(CGContextRef ctx, CGRect rect) {
+        CGContextSetFillColorWithColor(ctx, [UIColor colorWithWhite:0 alpha:0.5].CGColor);
+        CGContextAddPath(ctx, [UIBezierPath bezierPathWithRoundedRect:rect cornerRadius:radius].CGPath);
+        CGContextFillPath(ctx);
+    }] resizableImageWithCapInsets:UIEdgeInsetsMake(radius, radius, radius, radius)];
+    
+    // Create view
+    UIView *bezieView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, radius * 3, radius * 3)];
+    
+}
+
+@end
