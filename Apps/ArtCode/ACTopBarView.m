9deb4377a53d4b3f704359eb50d6d4e3786250b2
Nikso <thenikso@gmail.com>
2011-07-30 10:53:28 +0200

    Increased hit test area for top bar controls.

--- Apps/ArtCode/ACTopBarView.m
+++ Apps/ArtCode/ACTopBarView.m
@@ -10,22 +10,16 @@
 
 @implementation ACTopBarView
 
-- (id)initWithFrame:(CGRect)frame
+- (UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event
 {
-    self = [super initWithFrame:frame];
-    if (self) {
-        // Initialization code
-    }
-    return self;
+    if (![self pointInside:point withEvent:event])
+        return nil;
+    
+    // Effectively increase hit test area for subviews that are all layed out
+    // on the x direction.
+    CGRect bounds = self.bounds;
+    point.y = bounds.origin.y + bounds.size.height / 2;
+    return [super hitTest:point withEvent:event];
 }
-
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect
-{
-    // Drawing code
-}
-*/
 
 @end
