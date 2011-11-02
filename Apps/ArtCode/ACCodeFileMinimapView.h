bd269385e0190c69ff35d242d5e33eb3f1702ccd
Nicola Peduzzi <thenikso@gmail.com>
2011-11-02 14:22:34 +0100

    ACCodeFileMinimapView data source version.

--- Apps/ArtCode/ACCodeFileMinimapView.h
+++ Apps/ArtCode/ACCodeFileMinimapView.h
@@ -8,6 +8,44 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ACCodeFileMinimapView : UIView
+@class ACCodeFileMinimapView;
+
+@protocol ACCodeFileMinimapViewDataSource <NSObject>
+@required
+
+/// Returns the number of lines that the minimap has to display.
+- (NSUInteger)numberOfLinesForCodeFileMinimapView:(ACCodeFileMinimapView *)minimapView;
+
+/// Returns the length of the line at the given index as a number between 0 and 1, where 1 is the 
+/// length of the longest line. If given, the lineColor will be used instead of the default color
+/// for the line.
+- (CGFloat)codeFileMinimapView:(ACCodeFileMinimapView *)minimapView lenghtOfLineAtIndex:(NSUInteger)lineIndex applyColor:(UIColor **)lineColor;
+
+@end
+
+@interface ACCodeFileMinimapView : UIScrollView
+
+#pragma mark - Providing Data
+
+@property (weak, nonatomic) id<ACCodeFileMinimapViewDataSource> dataSource;
+
+- (void)reloadAllData;
+
+#pragma mark - Managing Minimap Style
+
+/// A view positioned as the background.
+@property (strong, nonatomic) UIView *backgroundView;
+
+/// The thickness of the uniform line representing a text line. Default 1, the minimum.
+@property (nonatomic) CGFloat lineHeight;
+
+/// The gap between two lines. Default 1, the minimum.
+@property (nonatomic) CGFloat lineGap;
+
+/// The default line color. If nil, black will be used.
+@property (strong, nonatomic) UIColor *lineColor;
+
+/// The color of the shadow for a line. Default nil meaning that lines will have no shadows.
+@property (strong, nonatomic) UIColor *lineShadowColor;
 
 @end
--- Apps/ArtCode/ACCodeFileMinimapView.m
+++ Apps/ArtCode/ACCodeFileMinimapView.m
@@ -7,25 +7,157 @@
 //
 
 #import "ACCodeFileMinimapView.h"
+#import <QuartzCore/QuartzCore.h>
 
-@implementation ACCodeFileMinimapView
+#define LINES_PER_TILE 200
+
+@interface ACCodeFileMinimapViewContent : UIView
+@property (copy, nonatomic) void (^customDrawRectBlock)(CGRect rect);
+@end
+
+@implementation ACCodeFileMinimapView {
+    ACCodeFileMinimapViewContent *_contentView;
+}
+
+#pragma mark - Properties
+
+@synthesize dataSource;
+@synthesize backgroundView;
+@synthesize lineHeight, lineGap, lineColor, lineShadowColor;
+
+- (void)setDataSource:(id<ACCodeFileMinimapViewDataSource>)aDataSource
+{
+    if (aDataSource == dataSource)
+        return;
+    
+    [self willChangeValueForKey:@"dataSource"];
+    dataSource = aDataSource;
+    [self reloadAllData];
+    [self didChangeValueForKey:@"dataSource"];
+}
+
+- (CGFloat)lineHeight
+{
+    if (lineHeight < 1)
+        lineHeight = 1;
+    return lineHeight;
+}
+
+- (void)setLineHeight:(CGFloat)height
+{
+    if (height == lineHeight)
+        return;
+    
+    [self willChangeValueForKey:@"lineHeight"];
+    lineHeight = height;
+    [self reloadAllData];
+    [self didChangeValueForKey:@"lineHeight"];
+}
+
+- (CGFloat)lineGap
+{
+    if (lineGap < 1)
+        lineGap = 1;
+    return lineGap;
+}
+
+- (void)setLineGap:(CGFloat)gap
+{
+    if (gap == lineGap)
+        return;
+    
+    [self willChangeValueForKey:@"lineGap"];
+    lineGap = gap;
+    [self reloadAllData];
+    [self didChangeValueForKey:@"lineGap"];
+}
+
+#pragma mark - View Methods
 
 - (id)initWithFrame:(CGRect)frame
 {
-    self = [super initWithFrame:frame];
-    if (self) {
-        // Initialization code
-    }
+    if ((self = [super initWithFrame:frame]))
+        return nil;
+    
+    _contentView = [[ACCodeFileMinimapViewContent alloc] initWithFrame:(CGRect){ CGPointZero, frame.size }];
+    _contentView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    __weak ACCodeFileMinimapView *this = self;
+    _contentView.customDrawRectBlock = ^(CGRect rect) {
+        // This method will be called for every tile of _contentView and rect will be the rect of the tile.
+        // Calculate index of first line to render and validate it
+        CGFloat totalLineHeight = this.lineHeight + this.lineGap;
+        NSUInteger lineIndex = rect.origin.y * totalLineHeight;
+        NSUInteger lineCount = [this.dataSource numberOfLinesForCodeFileMinimapView:this];
+        if (lineIndex >= lineCount)
+            return;
+        
+        // Calculate number of lines to render
+        lineCount = MIN((lineCount - lineIndex), rect.size.height * totalLineHeight);
+        
+        // Setup shadow
+        CGContextRef context = UIGraphicsGetCurrentContext();
+        if (this.lineShadowColor != nil)
+            CGContextSetShadowWithColor(context, CGSizeMake(1, 1), 0, this.lineShadowColor.CGColor);
+        
+        // Render lines
+        UIColor *color, *lastColor = this.lineColor;
+        CGFloat lineOffset = (fabsf(this.lineHeight) != this.lineHeight || (NSInteger)this.lineHeight % 2) ? 0 : 0.5;
+        CGPoint lineEndPoint;
+        for (; lineIndex < lineCount; ++lineIndex)
+        {
+            // Compute line width and color
+            color = this.lineColor;
+            lineEndPoint = CGPointMake([this.dataSource codeFileMinimapView:this lenghtOfLineAtIndex:lineIndex applyColor:&color] * rect.size.width, lineIndex * totalLineHeight + lineOffset);
+            
+            // Draw previous path of lines if with different color
+            if (lastColor != color)
+            {
+                CGContextSetStrokeColorWithColor(context, lastColor.CGColor);
+                CGContextStrokePath(context);
+                lastColor = color;
+            }
+            
+            // Add line to context path
+            CGContextMoveToPoint(context, 0, lineEndPoint.y);
+            CGContextAddLineToPoint(context, lineEndPoint.x, lineEndPoint.y);
+        }
+        
+        // Rendering last line group
+        CGContextSetStrokeColorWithColor(context, lastColor.CGColor);
+        CGContextStrokePath(context);
+    };
+    [self addSubview:_contentView];
+    
     return self;
 }
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
+#pragma mark - Data Methods
+
+- (void)reloadAllData
+{
+    ECASSERT(dataSource != nil);
+    
+    self.contentSize = CGSizeMake(UIEdgeInsetsInsetRect(self.frame, self.contentInset).size.width, [self.dataSource numberOfLinesForCodeFileMinimapView:self] * (self.lineHeight + self.lineGap));
+    [(CATiledLayer *)_contentView.layer setTileSize:CGSizeMake(self.contentSize.width, LINES_PER_TILE * (self.lineHeight + self.lineGap))];
+    [_contentView setNeedsDisplay];
+}
+
+@end
+
+
+@implementation ACCodeFileMinimapViewContent
+
+@synthesize customDrawRectBlock;
+
++ (Class)layerClass
+{
+    return [CATiledLayer class];
+}
+
 - (void)drawRect:(CGRect)rect
 {
-    // Drawing code
+    if (customDrawRectBlock)
+        customDrawRectBlock(rect);
 }
-*/
 
 @end
