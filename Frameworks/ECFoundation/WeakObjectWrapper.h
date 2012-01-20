2a8ec33a67fbf1ffd7aa26b2d8af89b2a75062bf
Uri Baghin <skerfat@gmail.com>
2012-01-20 12:59:38 +0100

    Added ECWeakArray.

---
+++ Frameworks/ECFoundation/WeakObjectWrapper.h
@@ -0,0 +1,17 @@
+//
+//  WeakObjectWrapper.h
+//  ECFoundation
+//
+//  Created by Uri Baghin on 1/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WeakObjectWrapper : NSObject
+{
+    @package
+    __weak id object;
+}
++ (WeakObjectWrapper *)wrapperWithObject:(id)object;
+@end
