50e464af10034dfbcc0ee963f4829fb12a89fe44
Nicola Peduzzi <thenikso@gmail.com>
2012-03-12 09:48:34 +0100

    Fixed bugs with ArtCodeURL detection of bookmarks and remotes URLS.

--- Apps/ArtCode/ArtCodeURL.m
+++ Apps/ArtCode/ArtCodeURL.m
@@ -18,8 +18,8 @@ static NSString * const ProjectsDirectoryName = @"LocalProjects";
 static NSString * const artCodeURLScheme = @"artcode";
 
 NSString * const artCodeURLProjectListPath = @"projects";
-NSString * const artCodeURLProjectBookmarkListPath = @"bookmarks";
-NSString * const artCodeURLProjectRemoteListPath = @"remotes";
+NSString * const artCodeURLProjectBookmarkListPath = @"/bookmarks";
+NSString * const artCodeURLProjectRemoteListPath = @"/remotes";
 
 
 @interface ArtCodeURL ()
@@ -33,19 +33,21 @@ NSString * const artCodeURLProjectRemoteListPath = @"remotes";
 + (NSURL *)artCodeURLWithProject:(ACProject *)project item:(ACProjectItem *)item path:(NSString *)path
 {
     NSString *URLString = nil;
+    if (path && ![path hasPrefix:@"/"])
+        path = [@"/" stringByAppendingString:path];
     if (item)
     {
         ECASSERT(project);
         ECASSERT(item.project == project);
-        URLString = [NSString stringWithFormat:@"%@://%@-%@/%@", artCodeURLScheme, [project UUID], [item UUID], path ?  path : @""];
+        URLString = [NSString stringWithFormat:@"%@://%@-%@%@", artCodeURLScheme, [project UUID], [item UUID], path ? path : @""];
     }
     else if (project)
     {
-        URLString = [NSString stringWithFormat:@"%@://%@/%@", artCodeURLScheme, [project UUID], path ?  path : @""];
+        URLString = [NSString stringWithFormat:@"%@://%@%@", artCodeURLScheme, [project UUID], path ? path : @""];
     }
     else if (path)
     {
-        URLString = [NSString stringWithFormat:@"%@://%@", artCodeURLScheme, path];
+        URLString = [NSString stringWithFormat:@"%@:/%@", artCodeURLScheme, path];
     }
     else 
     {
@@ -104,8 +106,6 @@ NSString * const artCodeURLProjectRemoteListPath = @"remotes";
     return [self.scheme isEqualToString:artCodeURLScheme];
 }
 
-#warning TODO controlling the path is not sufficient, there has to be a check on uuids
-
 - (BOOL)isArtCodeProjectsList
 {
     return [self.host isEqualToString:artCodeURLProjectListPath];
@@ -113,16 +113,17 @@ NSString * const artCodeURLProjectRemoteListPath = @"remotes";
 
 - (BOOL)isArtCodeProjectBookmarksList
 {
-    return [self.path isEqualToString:artCodeURLProjectBookmarkListPath];
+    return [self.host length] == 36 && [self.path isEqualToString:artCodeURLProjectBookmarkListPath];
 }
 
 - (BOOL)isArtCodeProjectRemotesList
 {
-    return [self.path isEqualToString:artCodeURLProjectRemoteListPath];
+    return [self.host length] == 36 && [self.path isEqualToString:artCodeURLProjectRemoteListPath];
 }
 
 - (NSArray *)artCodeUUIDs
 {
+    // TODO cache?
     static NSRegularExpression *uuidRegExp = nil;
     if (!uuidRegExp)
         uuidRegExp = [NSRegularExpression regularExpressionWithPattern:@"(?:([\\da-f]{8}-[\\da-f]{4}-[\\da-f]{4}-[\\da-f]{4}-[\\da-f]{12})-?)+" options:NSRegularExpressionCaseInsensitive error:NULL];
