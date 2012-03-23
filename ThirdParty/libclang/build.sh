633d4ba064a489b4dfc82637b55ae672d626346f
Uri Baghin <skerfat@gmail.com>
2012-03-23 14:23:07 +0100

    Updated libclang build script.

--- ThirdParty/libclang/build.sh
+++ ThirdParty/libclang/build.sh
@@ -1,16 +1,15 @@
+#!/bin/sh -eu
+
 LOG="build.log"
 PLATFORMS_ROOT="/Applications/Xcode.app/Contents/Developer/Platforms/"
 ARCHS="i386 armv7"
-SDKVERSION="5.0"
+SDKVERSION="5.1"
 OSXSDKVERSION="10.7"
 
-set -e
-set -u
 
 ROOT_DIRECTORY=`pwd`
 LOG=${ROOT_DIRECTORY}/${LOG}
 
-trap "echo '-> Build complete!'; echo '-> Last lines of log:'; echo '----------------------------------------'; tail ${LOG}; exit" INT TERM EXIT
 
 #empty out previous log
 echo > ${LOG}
@@ -18,7 +17,7 @@ echo > ${LOG}
 echo "-> Logging to ${LOG}"
 
 if [ ! -e llvm ];
-then
+    then
     echo "-> Checking out llvm source tree..."
     svn co http://llvm.org/svn/llvm-project/llvm/trunk llvm >> "${LOG}" 2>&1
     cd llvm/tools
@@ -34,7 +33,7 @@ else
 fi
 
 if [ ! -e BuildTools ];
-then
+    then
     echo "-> Building native build tools..."
     mkdir BuildTools
     cd BuildTools
@@ -48,32 +47,32 @@ fi
 for ARCH in ${ARCHS}
 do
     if [ "${ARCH}" == "i386" ];
-    then
-      PLATFORM="iPhoneSimulator"
-      CFLAGS="-mmacosx-version-min=${OSXSDKVERSION}"
-  else
-      PLATFORM="iPhoneOS"
-      CFLAGS="-miphoneos-version-min=${SDKVERSION}"
-  fi
-  echo "-> Building ${PLATFORM} :"
-  if [ ! -e build-${PLATFORM} ];
-    then
-      mkdir build-${PLATFORM}
+        then
+        PLATFORM="iPhoneSimulator"
+        CFLAGS="-mmacosx-version-min=${OSXSDKVERSION}"
+    else
+        PLATFORM="iPhoneOS"
+        CFLAGS="-miphoneos-version-min=${SDKVERSION}"
     fi
-  if [ ! -e install-${PLATFORM} ];
-    then
-      mkdir install-${PLATFORM}
+    echo "-> Building ${PLATFORM} :"
+    if [ ! -e build-${PLATFORM} ];
+        then
+        mkdir build-${PLATFORM}
+    fi
+    if [ ! -e install-${PLATFORM} ];
+        then
+        mkdir install-${PLATFORM}
     fi
     cd build-${PLATFORM}
-        
+
     export DEVROOT=${PLATFORMS_ROOT}${PLATFORM}.platform/Developer
-    export SDKROOT=${DEVROOT}/SDKs/${PLATFORM}5.0.sdk
+    export SDKROOT=${DEVROOT}/SDKs/${PLATFORM}${SDKVERSION}.sdk
     export CFLAGS="-I${ROOT_DIRECTORY} -isysroot ${SDKROOT} -arch ${ARCH} ${CFLAGS}"
     export CXXFLAGS="${CFLAGS}"
     export LDFLAGS="-lstdc++"
-    
+
     if [ "${ARCH}" != "i386" ];
-    then
+        then
         export CC=$DEVROOT/usr/bin/clang
         export BUILD_CC=$DEVROOT/usr/bin/clang
         export BUILD_CXX=$DEVROOT/usr/bin/clang
@@ -85,36 +84,56 @@ do
         export NM=$DEVROOT/usr/bin/nm
         export CXXCPP="$DEVROOT/usr/bin/clang -E"
     fi
-    
+
     if [ ! -e include/llvm/Config/config.h ];
-    then
+        then
         echo "---> Configuring..."
         ../llvm/configure --enable-optimized --disable-shared --disable-docs --host=${ARCH}-apple-darwin11 --prefix=${ROOT_DIRECTORY}/install-${PLATFORM} >> "${LOG}" 2>&1
     else
         echo "---> config.h found, skipping configuring"
     fi
     
+    SUCCESS=0
+
+    echo "---> Building for ${PLATFORM}..."
     if [ "${ARCH}" == "i386" ];
-    then
-        echo "---> Building..."
-        if ! make TOOL_VERBOSE=1 >> "${LOG}" 2>&1;
         then
-            echo "---> Build failed, probably because of a broken llvm-tblgen, copying from native build tools and trying again"
-            cp ../BuildTools/Release/bin/llvm-tblgen Release+Asserts/bin/
+
+        # i386 tblgen are broken and make the build process fail several times, every time we copy the x86_64 versions over and retry
+
+        for i in {1..5}
+        do
             if ! make TOOL_VERBOSE=1 >> "${LOG}" 2>&1;
-            then
-                echo "---> Build failed again, probably because of a broken clang-tblgen, copying from native build tools and trying again"
+                then
+                cp ../BuildTools/Release/bin/llvm-tblgen Release+Asserts/bin/
                 cp ../BuildTools/Release/bin/clang-tblgen Release+Asserts/bin/
-                make TOOL_VERBOSE=1 >> "${LOG}" 2>&1
+            else
+                SUCCESS=1
+                break
             fi
-        fi
+        done
+
     else
-        echo "---> Building..."
-        make TOOL_VERBOSE=1 >> "${LOG}" 2>&1
+        if make TOOL_VERBOSE=1 >> "${LOG}" 2>&1;
+            then
+            SUCCESS=1
+        fi
     fi
-    echo "---> Installing to ${ROOT_DIRECTORY}/install-${PLATFORM} ..."
-    make install TOOL_VERBOSE=1 >> "${LOG}" 2>&1
+    
+    if [ "${SUCCESS}" != "0" ];
+        then
+        echo "---> Installing to ${ROOT_DIRECTORY}/install-${PLATFORM} ..."
+        if ! make install TOOL_VERBOSE=1 >> "${LOG}" 2>&1;
+            then
+            SUCCESS=0
+        fi
+    fi
     cd ..
+    if [ "${SUCCESS}" == "0" ];
+        then
+        echo "---> Build failed! Check ${LOG} for details."
+        exit
+    fi
 done
 
 
@@ -123,12 +142,15 @@ cp -r install-iPhoneOS/include .
 
 echo "-> Creating fat libraries..."
 if [ ! -e lib ];
-then
+    then
     mkdir lib
 fi
-cd install-iPhoneOS
 
+cd install-iPhoneOS
 for FILE in lib/*.a
 do
-    lipo -create ${FILE} ../install-iPhoneSimulator/${FILE} -output ../${FILE}
+    lipo -create ${FILE} ../install-iPhoneSimulator/${FILE} -output ../${FILE};
 done
+cd ..
+
+echo '-> Build completed successfully!'
