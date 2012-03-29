c56c561f77349993c5486f1afca8a343c7ebb9be
Uri Baghin <skerfat@gmail.com>
2012-03-29 14:16:18 +0200

    Created test targets for CodeBuffer.

---
+++ Apps/CodeBufferTests/CodeBufferSpec.m
@@ -0,0 +1,13 @@
+//
+//  CodeBufferTests.m
+//  CodeBufferTests
+//
+//  Created by Uri Baghin on 3/29/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Kiwi.h>
+
+SPEC_BEGIN(CodeBufferSpec)
+
+SPEC_END
