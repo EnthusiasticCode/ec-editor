317cc9b2049903fe8a21a66cd9fe9407b6459cc3
Uri Baghin <skerfat@gmail.com>
2012-06-27 13:02:47 +0200

    Added diff-match-patch.

---
+++ Apps/ArtCode/TMSymbol.h
@@ -0,0 +1,13 @@
+//
+//  TMSymbol.h
+//  ArtCode
+//
+//  Created by Uri Baghin on 27/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TMSymbol : NSObject
+
+@end
---
+++ Apps/ArtCode/TMSymbol.m
@@ -0,0 +1,13 @@
+//
+//  TMSymbol.m
+//  ArtCode
+//
+//  Created by Uri Baghin on 27/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "TMSymbol.h"
+
+@implementation TMSymbol
+
+@end
