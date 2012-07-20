e12163a2ab6ecd401a62f59b55d29c863685c8c3
Uri Baghin <skerfat@gmail.com>
2012-07-20 12:11:02 +0200

    Implemented ArtCodeProjectSet without file system monitoring.

--- Apps/ArtCode/ArtCodeProjectSet.h
+++ Apps/ArtCode/ArtCodeProjectSet.h
@@ -14,14 +14,6 @@
 
 + (ArtCodeProjectSet *)defaultSet;
 
-/// The location of the project set on the filesystem
-@property (atomic, strong, readonly) NSURL *fileURL;
-
-/// Sets up and tears down the filesystem change observing machinery
-/// setUp and tearDown calls must be balanced
-- (void)setUp;
-- (void)tearDown;
-
 - (void)addNewProjectWithName:(NSString *)name completionHandler:(void(^)(ArtCodeProject *project))completionHandler;
 
 - (void)removeProject:(ArtCodeProject *)project completionHandler:(void(^)(NSError *error))completionHandler;
