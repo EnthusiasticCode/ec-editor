8a5064e3995e52350b6af241aa74337d35115f53
Uri Baghin <skerfat@gmail.com>
2012-08-01 17:00:06 +0200

    Implemented saving of bookmarks in extended attributes.

--- Apps/ArtCode/TextFile.m
+++ Apps/ArtCode/TextFile.m
@@ -7,6 +7,13 @@
 //
 
 #import "TextFile.h"
+#import <sys/xattr.h>
+
+static NSString * const _contentKey = @"TextFileContent";
+static NSString * const _bookmarksKey = @"TextFileBookmarks";
+
+static const char * const _bookmarksXattrName = "TextFileBookmarks";
+static size_t _bookmarksXattrMaxSize = 32 * 1024; // 32 kB
 
 @implementation TextFile {
   NSMutableIndexSet *_bookmarks;
@@ -15,15 +22,64 @@
 #pragma mark - UIDocument
 
 - (BOOL)loadFromContents:(id)contents ofType:(NSString *)typeName error:(NSError *__autoreleasing *)outError {
-  if (![contents isKindOfClass:[NSData class]]) {
+  if (![contents isKindOfClass:[NSDictionary class]]) {
     return NO;
   }
-  self.content = [[NSString alloc] initWithData:contents encoding:NSUTF8StringEncoding];
+  NSDictionary *contentsDictionary = contents;
+  self.content = [[NSString alloc] initWithData:[contentsDictionary objectForKey:_contentKey] encoding:NSUTF8StringEncoding];
+  id bookmarksData = [contentsDictionary objectForKey:_bookmarksKey];
+  if (bookmarksData && bookmarksData != [NSNull null]) {
+    self.bookmarks = [NSKeyedUnarchiver unarchiveObjectWithData:bookmarksData];
+  }
   return YES;
 }
 
 - (id)contentsForType:(NSString *)typeName error:(NSError *__autoreleasing *)outError {
-  return [self.content dataUsingEncoding:NSUTF8StringEncoding];
+  id bookmarksData = [NSNull null];
+  if (self.bookmarks) {
+    bookmarksData = [NSKeyedArchiver archivedDataWithRootObject:self.bookmarks];
+  }
+  return @{ _contentKey : [self.content dataUsingEncoding:NSUTF8StringEncoding], _bookmarksKey : bookmarksData };
+}
+
+- (BOOL)readFromURL:(NSURL *)url error:(NSError *__autoreleasing *)outError {
+  NSData *content = [NSData dataWithContentsOfURL:url options:0 error:outError];
+  if (!content) {
+    return NO;
+  }
+  void *bookmarksBytes = malloc(_bookmarksXattrMaxSize);
+  ssize_t bookmarksBytesCount = getxattr(url.path.fileSystemRepresentation, _bookmarksXattrName, &bookmarksBytes, _bookmarksXattrMaxSize, 0, 0);
+  id bookmarksData = [NSNull null];
+  if (bookmarksBytesCount != -1) {
+    bookmarksData = [NSData dataWithBytesNoCopy:bookmarksBytes length:bookmarksBytesCount freeWhenDone:YES];
+  } else {
+    free(bookmarksBytes);
+  }
+  __block BOOL success;
+  dispatch_sync(dispatch_get_main_queue(), ^{
+    success = [self loadFromContents:@{ _contentKey : content, _bookmarksKey : bookmarksData } ofType:nil error:outError];
+  });
+  return success;
+}
+
+- (BOOL)writeContents:(id)contents toURL:(NSURL *)url forSaveOperation:(UIDocumentSaveOperation)saveOperation originalContentsURL:(NSURL *)originalContentsURL error:(NSError *__autoreleasing *)outError {
+  if (![contents isKindOfClass:[NSDictionary class]]) {
+    return NO;
+  }
+  NSDictionary *contentsDictionary = contents;
+  NSData *fileContents = [contentsDictionary objectForKey:_contentKey];
+  if (!fileContents) {
+    return NO;
+  }
+  id bookmarksData = [contentsDictionary objectForKey:_bookmarksKey];
+  BOOL success = [fileContents writeToURL:url options:NSDataWritingAtomic error:outError];
+  if (!success) {
+    return NO;
+  }
+  if (bookmarksData && bookmarksData != [NSNull null]) {
+    success = !setxattr(url.path.fileSystemRepresentation, _bookmarksXattrName, [bookmarksData bytes], [bookmarksData length], 0, 0);
+  }
+  return success;
 }
 
 #pragma mark - Public Methods
