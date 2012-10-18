6edf04eae069a24776bb22e8bea6e94edf781aba
Nicola Peduzzi <thenikso@gmail.com>
2012-10-18 16:50:45 +0200

    Added directory download to ReactiveConnection

--- Apps/ArtCode/ReactiveConnection.h
+++ Apps/ArtCode/ReactiveConnection.h
@@ -40,12 +40,13 @@ enum ReactiveConnectionStatus {
 /// A subscribable that returns \c RACTuple of path to array of dictionaries with directory item informations
 - (RACSubscribable *)directoryContents;
 - (void)changeToDirectory:(NSString *)path;
+- (RACSubscribable *)directoryContentsForDirectory:(NSString *)path;
 
 #pragma mark Managin file transfers
 
 /// Returns a subscribable that send next when a download update is available.
 /// An update can be either an NSNumber containing the percentage of download complete of an NSURL of the localy downloaded file.
-- (RACSubscribable *)downloadFileWithRemotePath:(NSString *)remotePath isDirectory:(BOOL)isDirecotry;
+- (RACSubscribable *)downloadFileWithRemotePath:(NSString *)remotePath isDirectory:(BOOL)isDirectory;
 
 /// The local URL will be querried to see if it's a directory to enable recursive upload.
 /// Returns a subscribable that send next when an upload update is available.
