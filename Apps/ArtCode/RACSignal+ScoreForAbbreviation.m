b6feb991a57e990121d3802b0ce99cc5177a0098
Uri Baghin <skerfat@gmail.com>
2013-01-12 19:22:16 +0100

    Implement filterArraySignalByAbbreviation:extrapolateTargetStringBlock:.

--- Apps/ArtCode/RACSignal+ScoreForAbbreviation.m
+++ Apps/ArtCode/RACSignal+ScoreForAbbreviation.m
@@ -8,10 +8,15 @@
 
 #import "RACSignal+ScoreForAbbreviation.h"
 
+#import "NSArray+ScoreForAbbreviation.h"
+
 @implementation RACSignal (ScoreForAbbreviation)
 
 - (RACSignal *)filterArraySignalByAbbreviation:(RACSignal *)abbreviationSignal extrapolateTargetStringBlock:(NSString *(^)(id))targetStringBlock {
-	
+	NSParameterAssert(abbreviationSignal != nil);
+	return [RACSignal combineLatest:@[ self, abbreviationSignal ] reduce:^(NSArray *array, NSString *abbreviation){
+		return [array sortedArrayUsingScoreForAbbreviation:abbreviation extrapolateTargetStringBlock:targetStringBlock];
+	}];
 }
 
 @end
